24a27c225341d3505ff1e8cea9e3c6f4f9bb6a37
Jose Villarroel <oscarclarke9900@gmail.com>
2018-11-22 15:32:31 -0400

    https commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 var Message = require('./models/message');
 var indexRoutes = require('./routes/index');
+var http = require('http');
+var enforce = require('express-sslify');
 
 var dburl = process.env.DATABASEPORT || 'mongodb://localhost:27017/portfolio';
 var port = process.env.PORT || 3000;
@@ -10,12 +12,13 @@ var port = process.env.PORT || 3000;
 mongoose.connect(dburl, { useNewUrlParser: true });
 var app = express();
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use("/", indexRoutes);
 
-app.listen(port, function(){
-  console.log("Portfolio is up!");
+http.createServer(app).listen(port, function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
