70a11d44aafeb8cacbf356aafa0134b23fe7a24b
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-09 00:08:08 -0400

    added better handling to posting the contact form, shows a spinner while captcha is being validated and no longer displays form once form has be submitted successfully

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,36 +1,42 @@
-document.getElementById('postForm').addEventListener('submit', submitForm);
+(function() {
 
-function submitForm(e) {
-  e.preventDefault();
+  document.getElementById('postForm').addEventListener('submit', submitForm);
 
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const message = document.querySelector('#message').value;
-  const captcha = document.querySelector('#g-recaptcha-response').value;
+  function submitForm(e) {
+    e.preventDefault();
+    document.querySelector('.ld-spinner').style.display = 'inline-block';
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+    const captcha = document.querySelector('#g-recaptcha-response').value;
 
-  fetch('/', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json, text/plain, */*',
-      'Content-type': 'application/json'
-    },
-    body:JSON.stringify({name: name, email: email, message: message, captcha: captcha})
-  })
-  .then((res) => res.json())
-  .then((data) => {
-    if (data.success == false) {
-      swal({
-        type: 'error',
-        title: 'Captcha Error',
-        text: 'Something went wrong! '+data.msg+'!',
-      })
-    }
-    if (data.success == true) {
-      swal(
-        'Done!',
-        'Message submitted successfully!',
-        'success'
-      )
-    }
-  });
-}
+    fetch('/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-type': 'application/json'
+      },
+      body:JSON.stringify({name: name, email: email, message: message, captcha: captcha})
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success == false) {
+        swal({
+          type: 'error',
+          title: 'Captcha Error',
+          text: 'Something went wrong! '+data.msg+'!',
+        })
+        document.querySelector('.ld-spinner').style.display = 'none';
+      }
+      if (data.success == true) {
+        swal(
+          'Done!',
+          'Message submitted successfully!',
+          'success'
+        )
+        document.querySelector('#postForm').style.display = "none";
+        document.querySelector('.i-huge').style.display = "block";
+      }
+    });
+  }
+})();
